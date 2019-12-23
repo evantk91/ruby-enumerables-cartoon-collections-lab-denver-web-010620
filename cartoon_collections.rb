7512379dc2e6b2902cfb4c088965dd60b7ab2bbd
Evan Greer <etgreer91@gmail.com>
2019-12-23 08:30:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,15 @@ def long_planeteer_calls(planeteers)# code an argument here
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  while i < snacks.length do
+    snack = snacks[i]
+    if cheese_types.include?(snack)
+      return snack
+    end
+    i += 1
+  end
+
+  nil
 end
