1cc17409a8a45ca089f32a159c93bbae2c10785e
Evan Greer <etgreer91@gmail.com>
2019-12-23 08:07:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)# code an argument here
+  names
+  name_list = Hash.new
+  names.each_with_index{ |name, index|
+    name_list[name] = index
+    puts "#{index + 1}. #{name}"
+  }
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)# code an argument here
+  planeteers.map{ |name| name.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteers)# code an argument here
+  planeteers.any?{|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
